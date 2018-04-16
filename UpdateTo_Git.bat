4025d4772784af419ab31295ab3e1163a5076c97
Edward Arreola <earreola@logitech.com>
2018-04-16 15:02:20 -0700

    made changes

--- UpdateTo_Git.bat
+++ UpdateTo_Git.bat
@@ -2,7 +2,7 @@ git config credential.helper store
 
 git push https://github.com/earreola101/TDE_2018.git
 
-git checkout dev
+git checkout TDE_2018
 git add .
 git commit -am "made changes"
 git push
