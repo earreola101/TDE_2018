445685ba0f1fc38d66900ed90e00a89c19e07258
Edward Arreola <earreola@logitech.com>
2018-04-16 15:10:31 -0700

    made changes

--- UpdateTo_Git.bat
+++ UpdateTo_Git.bat
@@ -1,8 +1,6 @@
 git config --global credential.helper wincred
 git checkout https://github.com/earreola101/TDE_2018.git
 
-git checkout TDE_2018
-git add .
 git commit -am "made changes"
-git push
+
 pause
